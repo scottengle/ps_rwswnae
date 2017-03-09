bdc81ab2cac5ecd7f0fec7a5f4ce4fe6285ff0d4
Scott Engle <scottengle22@gmail.com>
2017-03-09 14:54:02 -0700

    Add query string parameter filtering for genre and allow for single book route

--- app.js
+++ app.js
@@ -9,18 +9,33 @@
 
   var db = mongoose.connect('mongodb://localhost/bookAPI');
 
-  bookRouter
-    .route('/books')
-      .get(function(req, res) {
-        Book.find(function(err, books) {
-          if(err) {
-            res.status(500).send(err);
-          } else {
-            res.json(books);
-          }
-        });
+  bookRouter.route('/books')
+    .get(function(req, res) {
+      var query = {};
+      if(req.query.genre) {
+        // Course author feels that this is a way of sanitizing user input. I disagree.
+        query.genre = req.query.genre;
+      }
+      Book.find(query, function(err, books) {
+        if(err) {
+          res.status(500).send(err);
+        } else {
+          res.json(books);
+        }
       });
+    });
 
+  bookRouter.route('/books/:bookId')
+    .get(function(req, res) {
+      Book.findById(req.params.bookId, function(err, book) {
+        if(err) {
+          res.status(500).send(err);
+        } else {
+          res.json(book);
+        }
+      });
+    });
+  
   app.use('/api', bookRouter);
 
   app.get('/', function(req, res){
