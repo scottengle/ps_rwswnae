232b6df98b04ca3963fe0aeedcffbcb345bd6385
Scott Engle <scottengle22@gmail.com>
2017-03-09 14:44:11 -0700

    wire up Mongoose and Mongo (module 2)

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # Course work for Pluralsight course "RESTful Web Services with Node.js and Express"
 
 https://app.pluralsight.com/library/courses/node-js-express-rest-web-services
+
+## Running The Application
+
+MongoDB is required:
+
+    > brew install mongo
+    > mongod
+
+Application start:
+
+    > gulp
--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 (function() {
-  var express = require('express'),
-      app = express(),
-      port = process.env.PORT || 3000;
   
+  var express = require('express'),
+      mongoose = require('mongoose'),
+      app = express(),
+      port = process.env.PORT || 3000,
+      bookRouter = express.Router(),
+      Book = require('./models/bookModel');
+
+  var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+  bookRouter
+    .route('/books')
+      .get(function(req, res) {
+        Book.find(function(err, books) {
+          if(err) {
+            res.status(500).send(err);
+          } else {
+            res.json(books);
+          }
+        });
+      });
+
+  app.use('/api', bookRouter);
+
   app.get('/', function(req, res){
     res.send('Welcome to my API');
   });
