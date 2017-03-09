d7fa170b5df93b22eda60ab9c174af6765ba150a
Scott Engle <scottengle22@gmail.com>
2017-03-09 15:26:37 -0700

    Add HTTP POST verb and clean up refactors (module 3)

--- app.js
+++ app.js
@@ -2,44 +2,20 @@
   
   var express = require('express'),
       mongoose = require('mongoose'),
+      bodyParser = require('body-parser'),
       app = express(),
       port = process.env.PORT || 3000,
-      bookRouter = express.Router(),
-      Book = require('./models/bookModel');
+      bookRouter = require('./routes/bookRoutes')();
 
   var db = mongoose.connect('mongodb://localhost/bookAPI');
-
-  bookRouter.route('/books')
-    .get(function(req, res) {
-      var query = {};
-      if(req.query.genre) {
-        // Course author feels that this is a way of sanitizing user input. I disagree.
-        query.genre = req.query.genre;
-      }
-      Book.find(query, function(err, books) {
-        if(err) {
-          res.status(500).send(err);
-        } else {
-          res.json(books);
-        }
-      });
-    });
-
-  bookRouter.route('/books/:bookId')
-    .get(function(req, res) {
-      Book.findById(req.params.bookId, function(err, book) {
-        if(err) {
-          res.status(500).send(err);
-        } else {
-          res.json(book);
-        }
-      });
-    });
   
-  app.use('/api', bookRouter);
+  app.use(bodyParser.urlencoded());
+  app.use(bodyParser.json());
+
+  app.use('/api/books', bookRouter);
 
   app.get('/', function(req, res){
-    res.send('Welcome to my API');
+    res.send('Unsupported route');
   });
 
   app.listen(port, function() {
