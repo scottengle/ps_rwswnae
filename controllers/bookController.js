03fb73be06dbba139f60068b985f2678b7238023
Scott Engle <scottengle22@gmail.com>
2017-03-09 18:54:14 -0700

    Add HATEOAS links to resources

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,6 +1,8 @@
 (function() {
   'use strict';
 
+  var urlencode = require('urlencode');
+
   var bookController = function(Book) {
 
     function getAllBooks(req, res) {
@@ -13,13 +15,25 @@
         if(err) {
           res.status(500).send(err);
         } else {
-          res.json(books);
+          var results = [];
+          books.forEach(function(elem, index, arr){
+            var newBook = elem.toJSON(); // strips out all mongoose special properties
+            newBook._links = {};
+            newBook._links.self = req.protocol + "://" + req.headers.host + '/api/books/' + newBook._id;
+            newBook._links.genre = req.protocol + "://" + req.headers.host + '/api/books?genre=' + urlencode(newBook.genre);
+            results.push(newBook);
+          });
+          res.json(results);
         }
       });
     }
     
     function getBook(req, res) {
-      res.json(req.book);
+      var result = req.book.toJSON(); // strips out all mongoose special properties
+      result._links = {};
+      result._links.self = req.protocol + "://" + req.headers.host + '/api/books/' + result._id;
+      result._links.similar = req.protocol + "://" + req.headers.host + '/api/books?genre=' + urlencode(result.genre);
+      res.json(result);
     }
 
     function createBook(req, res) {
